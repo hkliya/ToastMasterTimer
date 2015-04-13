269b6459e80ab6ab70d0b3997a4006ce9d7e277f
Seaborn Lee <seabornlee.cn@gmail.com>
2015-04-13 17:06:34 +0800

    Set time to 8 minutes

--- app/src/main/java/cn/seabornlee/toastmastertimer/MainActivity.java
+++ app/src/main/java/cn/seabornlee/toastmastertimer/MainActivity.java
@@ -38,6 +38,9 @@ public class MainActivity extends RoboActivity {
     @InjectView(R.id.btn_time_7mins)
     private Button btn_7_mins;
 
+    @InjectView(R.id.btn_time_8mins)
+    private Button btn_8_mins;
+
     @InjectView(R.id.btn_time_10mins)
     private Button btn_10_mins;
 
@@ -128,6 +131,13 @@ public class MainActivity extends RoboActivity {
             @Override
             public void onClick(View v) {
                 timer.start(7);
+            }
+        });
+
+        btn_8_mins.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer.start(8);
             }
         });
 
